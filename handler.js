c4d4e28ad685cfb86c1017fd9ba9d7de477f751f
Ivan Drondov <i.drondov@wnb.com.ua>
2018-04-21 19:59:45 +0300

    add basic parser

--- handler.js
+++ handler.js
@@ -1,38 +1,9 @@
 const petrovich = require('petrovich');
+const Parser = require('./Parser');
+
+const parser = new Parser();
 
 module.exports = async function handler(text) {
-    console.log(text);
+    const transactions = parser.parse(text);
+    console.log(transactions);
 };
-
-class Parser {
-    parse(text) {
-        return {
-            to: 123,
-            from: 321,
-            amount: 10,
-            currency: 'UAH',
-            description: 'за еду'
-        }
-    }
-}
-
-/**
- * Examples:
- * Дима должен мне 10 гривен за еду
- * Дима должен 10 за еду
- * Дима должен
- * (who) [должен] [to] (amount) [currency] [за (description)]
- * [who] [заплатил|закинул] [за (description)] (amount) [currency] на всех
- * 
- * Я заплатил за хавчик 500 гривен на всех
- * Я закинул 600 за ужин, а Ваня 50 на чаюху
- * 100 со всех за бенз мне
- * закинул за жеку 200 гривен
- * я закинул за жеку 200 гривен
- * дима закинул за жеку 1
- * 
- * Я закинул за хавло 500 гривен
- * Ваня на чаюху оставил 50
- * Жека ничего не закинул
- * 
- */
